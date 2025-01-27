c71498b8cb2feb73172bcee31817a35bec08862a
muhammedsiyadp <44540253+muhammedsiyadp@users.noreply.github.com>
2025-01-27 19:46:01 +0530

    update the code for opencv 4

--- LeGO-LOAM/include/utility.h
+++ LeGO-LOAM/include/utility.h
@@ -10,7 +10,8 @@
 
 #include "cloud_msgs/cloud_info.h"
 
-#include <opencv/cv.h>
+//#include <opencv/cv.h>
+#include <opencv2/opencv.hpp>
 
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
@@ -60,12 +61,12 @@ extern const string fileDirectory = "/tmp/";
 extern const bool useCloudRing = true; // if true, ang_res_y and ang_bottom are not used
 
 // VLP-16
-extern const int N_SCAN = 16;
-extern const int Horizon_SCAN = 1800;
-extern const float ang_res_x = 0.2;
-extern const float ang_res_y = 2.0;
-extern const float ang_bottom = 15.0+0.1;
-extern const int groundScanInd = 7;
+// extern const int N_SCAN = 16;
+// extern const int Horizon_SCAN = 1800;
+// extern const float ang_res_x = 0.2;
+// extern const float ang_res_y = 2.0;
+// extern const float ang_bottom = 15.0+0.1;
+// extern const int groundScanInd = 7;
 
 // HDL-32E
 // extern const int N_SCAN = 32;
@@ -74,6 +75,14 @@ extern const int groundScanInd = 7;
 // extern const float ang_res_y = 41.33/float(N_SCAN-1);
 // extern const float ang_bottom = 30.67;
 // extern const int groundScanInd = 20;
+
+extern const int N_SCAN = 32;
+extern const int Horizon_SCAN = 220;
+extern const float ang_res_x = 360.0/float(Horizon_SCAN);
+extern const float ang_res_y = 41.33/float(N_SCAN-1);
+extern const float ang_bottom = 30.67;
+extern const int groundScanInd = 20;
+
 
 // VLS-128
 // extern const int N_SCAN = 128;
